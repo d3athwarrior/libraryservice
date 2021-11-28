a11adf1d7438f4b6cb903aaa8d3b4712ee98059a
d3athwarrior <me@d3athwarrior.dev>
2021-11-28 10:56:20 +0530

    Add cors handling: allow all origins
    All origins are allowed since the application is going to run on local
    
    Signed-off-by: d3athwarrior <me@d3athwarrior.dev>

--- src/main/java/dev/d3athwarrior/libraryservice/LibraryserviceApplication.java
+++ src/main/java/dev/d3athwarrior/libraryservice/LibraryserviceApplication.java
@@ -2,12 +2,25 @@ package dev.d3athwarrior.libraryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LibraryserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryserviceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOriginPatterns("**");
+            }
+        };
     }
 
 }
