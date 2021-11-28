222e13e2f2ad5827c19fcd00b495ab0d4562b99a
d3athwarrior <me@d3athwarrior.dev>
2021-11-28 19:56:52 +0530

    Update dummy data
    Signed-off-by: d3athwarrior <me@d3athwarrior.dev>

--- databasescripts/app_user_dummy_data.sql
+++ databasescripts/app_user_dummy_data.sql
@@ -1,2 +1,8 @@
 INSERT INTO public.app_user (user_id, first_name, last_name)
 VALUES (1, 'Anish', 'Kumar');
+INSERT INTO public.app_user (user_id, first_name, last_name)
+VALUES (2, 'Test', 'User');
+INSERT INTO public.app_user (user_id, first_name, last_name)
+VALUES (3, 'Test', 'User 2');
+INSERT INTO public.app_user (user_id, first_name, last_name)
+VALUES (4, 'Test 3', 'User');
--- databasescripts/book_dummy_data.sql
+++ databasescripts/book_dummy_data.sql
@@ -8,3 +8,15 @@ INSERT INTO public.book (book_id, author_name, name, num_of_copies)
 VALUES (4, 'H G Wells', 'The Invisible Man', 5);
 INSERT INTO public.book (book_id, author_name, name, num_of_copies)
 VALUES (5, 'H G Wells', 'The War of the Worlds', 1);
+INSERT INTO public.book (book_id, author_name, name, num_of_copies)
+VALUES (7, 'J K Rowling', 'Harry Potter and The Chamber of Secrets', 4);
+INSERT INTO public.book (book_id, author_name, name, num_of_copies)
+VALUES (8, 'Brené Brown', 'Atlas of the Heart', 10);
+INSERT INTO public.book (book_id, author_name, name, num_of_copies)
+VALUES (9, 'Paul Bloom', 'The Sweet Spot', 2);
+INSERT INTO public.book (book_id, author_name, name, num_of_copies)
+VALUES (10, 'Jane Goodall', 'The Book of Hope', 3);
+INSERT INTO public.book (book_id, author_name, name, num_of_copies)
+VALUES (11, 'Susan Ashford', 'The Power of Flexing', 2);
+INSERT INTO public.book (book_id, author_name, name, num_of_copies)
+VALUES (12, 'Anne Helen Petersen and Charlie Warzel', 'Out of Office', 6);
